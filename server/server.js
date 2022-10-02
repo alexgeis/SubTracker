d9b1451dc9c49ab4791b5ebbace755004b653143
Alex Geis <siegxela@gmail.com>
2022-10-02 16:48:48 -0600

    update universal route path to homepage /

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ console.log(process.env.NODE_ENV);
 app.use(
 	express.static(path.join(__dirname, "..", "client", "build", "static"))
 );
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "client", "public", "index.html"));
 });
 if (process.env.NODE_ENV === "production") {
