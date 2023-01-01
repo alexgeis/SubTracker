c96efa00cefb17ddaf3729b043e24a4b35318564
Alex Geis <siegxela@gmail.com>
2023-01-01 14:10:42 -0700

    attempting to resolve apollo server issues - cont

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+/*
+DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
+*/
 
 mongoose.connect(
 	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/subscription-tracker",
@@ -8,6 +13,7 @@ mongoose.connect(
 	}
 );
 
-export default db = mongoose.connection;
+const db = mongoose.connection;
+export default db;
 
 // module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -4,21 +4,21 @@ import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import pkg from "body-parser";
 const { json } = pkg;
-import path from "path";
 import { authMiddleware } from "./utils/auth.js";
 
 import { typeDefs } from "./schemas/typeDefs.js";
 import { resolvers } from "./schemas/resolvers.js";
 import db from "./config/connection.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	// context: authMiddleware,
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,29 +34,40 @@ app.get("/", (req, res) => {
 if (process.env.NODE_ENV === "production") {
 	// app.use(express.static(path.join(__dirname, "../client/build")));
 
-	app.use(express.static(path.join(__dirname, "../client/build/static")));
+	// app.use(express.static(path.join(__dirname, "../client/build/static")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 	});
 }
 
-await server.start();
-// server.applyMiddleware({ app }); // depracated from apollo-server-3
-app.use(
-	"/graphql",
-	cors(),
-	json(),
-	expressMiddleware(server, {
-		context: authMiddleware,
-	})
-);
-
-db.once("open", () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startApolloServer = async () => {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
 	});
-});
+	await server.start();
+	// server.applyMiddleware({ app }); // depracated from apollo-server-3
+	app.use(
+		"/graphql",
+		cors(),
+		json(),
+		expressMiddleware(server, {
+			context: authMiddleware,
+		})
+	);
+	console.log("enter");
+	db.once("open", () => console.log("HEYYYYYY"));
+	// db.once("open", () => {
+	// 	app.listen(PORT, () => {
+	// 		console.log(`API server running on port ${PORT}!`);
+	// 		console.log(
+	// 			`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+	// 		);
+	// 	});
+	// });
+	console.log("exit");
+};
+startApolloServer();
 
 // const startApolloServer = async (typeDefs, resolvers) => {
 // 	await server.start();
