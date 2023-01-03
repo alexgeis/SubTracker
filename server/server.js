b1f78abc01605bc3b9ce9717ae5003f0082aafdf
Alex Geis <siegxela@gmail.com>
2023-01-03 11:37:22 -0700

    uncomment out the regular server.js (accidentally commented out the netlify/functions version)

--- server/server.js
+++ server/server.js
@@ -1,91 +1,91 @@
-// import express from "express";
-// import serverless from "serverless-http";
-// import { ApolloServer } from "@apollo/server";
-// import { expressMiddleware } from "@apollo/server/express4";
-// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-// import http from "http";
-// import cors from "cors";
-// import pkg from "body-parser";
-// const { json } = pkg;
-// import { authMiddleware } from "./utils/auth.js";
+import express from "express";
+import serverless from "serverless-http";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import http from "http";
+import cors from "cors";
+import pkg from "body-parser";
+const { json } = pkg;
+import { authMiddleware } from "./utils/auth.js";
 
-// import { typeDefs } from "./schemas/typeDefs.js";
-// import { resolvers } from "./schemas/resolvers.js";
-// import db from "./config/connection.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
+import { typeDefs } from "./schemas/typeDefs.js";
+import { resolvers } from "./schemas/resolvers.js";
+import db from "./config/connection.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const PORT = process.env.PORT || 3001;
-// const GRAPHQL_PORT = 4000;
+const PORT = process.env.PORT || 3001;
+const GRAPHQL_PORT = 4000;
 
-// const app = express();
+const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(
-// 	express.static(path.join(__dirname, "..", "client", "build", "static"))
-// );
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+	express.static(path.join(__dirname, "..", "client", "build", "static"))
+);
 
-// // routes
-// app.get("/", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-// });
+// routes
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+	});
+}
 
-// // connect to mongo db and spin up express server
-// db.once("open", () => {
-// 	// start express app
-// 	app.listen(PORT, () => {
-// 		console.log(`API server running on port ${PORT}!`);
-// 	});
-// });
+// connect to mongo db and spin up express server
+db.once("open", () => {
+	// start express app
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+	});
+});
 
-// // apollo server for graphql
-// const httpServer = http.createServer(app);
-// const server = new ApolloServer({
-// 	typeDefs,
-// 	resolvers,
-// 	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-// });
+// apollo server for graphql
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
 
-// await server.start();
+await server.start();
 
-// app.use(
-// 	"/graphql",
-// 	cors(),
-// 	json(),
-// 	expressMiddleware(server, {
-// 		context: authMiddleware,
-// 	})
-// );
+app.use(
+	"/graphql",
+	cors(),
+	json(),
+	expressMiddleware(server, {
+		context: authMiddleware,
+	})
+);
 
-// //start apollo graphql server
-// await new Promise((resolve) => {
-// 	httpServer.listen({ port: GRAPHQL_PORT }, resolve);
-// 	console.log(`Use GraphQL at http://localhost:${GRAPHQL_PORT}/graphql`);
-// });
+//start apollo graphql server
+await new Promise((resolve) => {
+	httpServer.listen({ port: GRAPHQL_PORT }, resolve);
+	console.log(`Use GraphQL at http://localhost:${GRAPHQL_PORT}/graphql`);
+});
 
-// const functionName = "server";
-// const routerBasePath =
-// 	process.env.NODE_ENV === "dev"
-// 		? `/${functionName}`
-// 		: `/.netlify/functions/${functionName}/`;
-// app.get(routerBasePath, (req, res) => {
-// 	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-// });
-// // handler export for lambda functions
-// // exports.handler = serverless(app);
-// const handler = serverless(app);
-// exports.handler = async (event, context) => {
-// 	const result = await handler(event, context);
-// 	return result;
-// };
+const functionName = "server";
+const routerBasePath =
+	process.env.NODE_ENV === "dev"
+		? `/${functionName}`
+		: `/.netlify/functions/${functionName}/`;
+app.get(routerBasePath, (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+// handler export for lambda functions
+// exports.handler = serverless(app);
+const handler = serverless(app);
+exports.handler = async (event, context) => {
+	const result = await handler(event, context);
+	return result;
+};
