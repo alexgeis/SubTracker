920f55f0ddcfe7a8e81feea7d7d6c7c174682254
Alex Geis <siegxela@gmail.com>
2023-01-03 17:33:05 -0700

    add railway app deployed homepage to package.json

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "..", "client", "public")));
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // routes
