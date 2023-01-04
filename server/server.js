5e4673bb23cef08972200890e43e16ff5a6df008
Alex Geis <siegxela@gmail.com>
2023-01-03 17:32:03 -0700

    update server/server.js to serve static files from the build/ folder, not just the static files within that folder

--- server/server.js
+++ server/server.js
@@ -26,9 +26,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-	express.static(path.join(__dirname, "..", "client", "build", "static"))
-);
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // routes
 app.get("/", (req, res) => {
